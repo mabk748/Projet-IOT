c971e5f1098ded2c89fb5801214181fe67653e45
Audrey Garcia <garcia_audrey@orange.fr>
2022-11-28 12:13:33 +0100

    Revision Equipe

--- Site Web/Front End/Equipe.php
+++ Site Web/Front End/Equipe.php
@@ -18,30 +18,66 @@
 
     <div class="relative">
         <form action="CV.php" method="GET">
-                <div id="personnep">
-                    <button type="submit" name="name" value="Gaye_DIAWARA">
-                        <img src="fantome1.png" width="100" height="100">
-                    </button>
-                    <a>Gaye Diawara</a>
-                </div>
-                <div id="personnei">
-                    <a>Audrey Garcia</a>
-                    <button type="submit" name="name" value="Audrey_GARCIA">
-                        <img src="fantome3.png" width="100" height="100">
-                    </button>
-                </div>
-                <div id="personnep">
-                    <button type="submit" name="name" value="Mohamed_ABOUAKIL">
-                        <img src="fantome4.png" width="100" height="100">
-                    </button>
-                    <a>Mohamed Abouakil</a>
-                </div>
-                <div id="personnei">
-                    <a>Quentin Lesniak</a>  
-                    <button type="submit" name="name" value="Quentin_LESNIAK">
-                        <img src="fantome2.png" width="100" height="100">
-                    </button>
-                </div>
+                <table id="personnep">
+                    <tr>
+                        <td rowspan="2">
+                            <button type="submit" name="name" value="Gaye_DIAWARA">
+                                <img src="fantome1.png" width="100" height="100">
+                            </button>
+                        </td>
+                        <td colspan="3">
+                            <a>Gaye Diawara</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">Chef de projet et fondateur de l'initiative HOMEotix</td>
+                    </tr>
+                </table>
+                <table id="personnei">
+                    <tr>
+                        <td colspan="3">
+                            <a>Audrey Garcia</a>
+                        </td>
+                        <td rowspan="2">
+                            <button type="submit" name="name" value="Audrey_GARCIA">
+                                <img src="fantome3.png" width="100" height="100">
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3"></td>
+                    </tr>
+                </table>
+                <table id="personnep">
+                    <tr>
+                        <td rowspan="2">
+                            <button type="submit" name="name" value="Mohamed_ABOUAKI">
+                                <img src="fantome4.png" width="100" height="100">
+                            </button>
+                        </td>
+                        <td colspan="3">
+                            <a>Mohamed Abouakil</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3"> Developpeur Front End</td>
+                    </tr>
+                </table>
+                <table id="personnei">
+                    <tr>
+                        <td colspan="3">
+                            <a>Quentin Lesniak</a>
+                        </td>
+                        <td rowspan="2">
+                            <button type="submit" name="name" value="Quentin_LESNIAK">
+                                <img src="fantome2.png" width="100" height="100">
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">Developpeur Back End</td>
+                    </tr>
+                </table>
     </div>
     
     <?php   require("footer.html"); ?>
