1080f963ea2853aa05bbe630ab251f388f667eba
gdwr40 <diawaragaye0@gmail.com>
2022-12-08 16:57:18 +0100

    Update MQTT_data_to-database.php

--- Site Web/Back End/MQTT_data_to-database.php
+++ Site Web/Back End/MQTT_data_to-database.php
@@ -1,12 +1,17 @@
 <?php
+require('vendor/autoload.php');
+
+use \PhpMqtt\Client\MqttClient;
+use \PhpMqtt\Client\ConnectionSettings;
+
 $server   = '192.168.43.59';
 $port     = 1883;
 $clientId = 'php_sql';
-
 $mqtt = new \PhpMqtt\Client\MqttClient($server, $port, $clientId);
 $mqtt->connect();
-$mqtt->subscribe('php-mqtt/client/test', function ($topic, $message, $retained, $matchedWildcards) {
-    echo sprintf("Received message on topic [%s]: %s\n", $topic, $message);
-}, 0);
+$mqtt->subscribe('home/+', function ($topic, $message, $retained, $matchedWi>
+{
+    echo sprintf("Received message on topic [%s]: %s\n", $topic, $message);}>
 $mqtt->loop(true);
 $mqtt->disconnect();
+
